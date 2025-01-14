667a8113890bbce550437ba22f9eef3b2f8d1f50
sid0402 <sagarwal437@gatech.edu>
2025-01-14 13:27:33 -0500

    adapted all page descriptions

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -12,8 +12,8 @@ const Main = (props) => (
     <Analytics />
     <ScrollToTop />
     <Helmet
-      titleTemplate="%s | Michael D'Angelo"
-      defaultTitle="Michael D'Angelo"
+      titleTemplate="%s | Siddhant Agarwal"
+      defaultTitle="Siddhant Agarwal"
       defer={false}
     >
       {props.title && <title>{props.title}</title>}
@@ -41,7 +41,7 @@ Main.defaultProps = {
   children: null,
   fullPage: false,
   title: null,
-  description: "Michael D'Angelo's personal website.",
+  description: "Siddhant Agarwal's personal website.",
 };
 
 export default Main;
